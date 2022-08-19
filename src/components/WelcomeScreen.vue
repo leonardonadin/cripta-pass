de4df72846bb7555c13f894ed7786e6ad740edd7
Leonardo Nadin <nadin.leonardo@gmail.com>
2022-08-19 20:05:53 -0300

    feat: :sparkles: adiciona gerenciamento das senhas
    Adiciona listagem das senhas; Edição dos grupos e itens que estão no arquivo; Faz o encrypt e decrypt das senhas;

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -10,15 +10,14 @@
                     />
                 </div>
 
-                <h1 class="display-2 font-weight-bold mb-3 text-center">
-                    Bem vindo ao CriptaPass
-                </h1>
-
-                <p class="subheading font-weight-regular">
-                    Antes de continuar informe a senha mestra para descriptografar os dados.
-                </p>
-
-                <br />
+                <div class="mb-5">
+                    <h1 class="display-2 font-weight-bold mb-3 text-center">
+                        Bem vindo ao CriptaPass
+                    </h1>
+                    <p class="subheading font-weight-regular">
+                        Antes de continuar informe a senha mestra para descriptografar os dados.
+                    </p>
+                </div>
 
                 <v-text-field
                     v-model="pass"
@@ -41,7 +40,7 @@
                     hide-details
                 ></v-switch>
 
-                <div v-if="isNewFile">
+                <div v-if="isNewFile" class="mb-5">
                     <v-btn
                         color="primary"
                         class="white--text"
@@ -64,7 +63,8 @@
                 <v-btn
                     color="primary"
                     class="white--text"
-                    @click="storePass">
+                    :disabled="isValidated()"
+                    @click="storeData">
                     Continuar
                 </v-btn>
             </v-col>
@@ -73,7 +73,8 @@
 </template>
 
 <script>
-    const { remote } = require('electron')
+    const { remote } = require('electron');
+    const fs = require('fs');
 
     export default {
         name: 'WelcomeScreen',
@@ -83,21 +84,37 @@
             directory: [],
             isNewFile: false,
         }),
+        mounted: function() {
+            var filePath = localStorage.getItem('file');
+            if(filePath) {
+                this.$router.push('/list');
+            }
+        },
         methods: {
-            storePass() {
-                console.log(this.pass);
-                console.log(this.file);
-                console.log(this.directory);
-                console.log(this.directory);
-                console.log(this.isNewFile);
-                // localStorage.setItem('pass', this.pass);
-                // this.$router.push('/about');
+            storeData() {
+                localStorage.setItem('pass', this.pass);
+                if(this.isNewFile) {
+                    var filePath = this.directory+'/data.json';
+                    fs.writeFileSync(filePath, JSON.stringify([]));
+                    localStorage.setItem('file', filePath);
+                } else {
+                    localStorage.setItem('file', this.file);
+                }
+
+                this.$router.push('/list');
             },
             async selectDirectory() {
                 var dir = await remote.dialog.showOpenDialog({
                     properties: ['openDirectory']
                 });
                 this.directory = dir.filePaths[0];
+            },
+            isValidated() {
+                if(this.isNewFile) {
+                    return this.pass.length === 0 || this.directory.length === 0;
+                } else {
+                    return this.pass.length === 0 || this.file.length === 0;
+                }
             }
         }
     }
